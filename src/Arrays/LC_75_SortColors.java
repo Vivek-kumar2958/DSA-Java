88639135937e653d9de4252882e47c2f0ce74cf7
vivek kumar <vivekkumar2958bxr@gmail.com>
2026-08-13 01:09:50 +0530

    Update Sort Colors solution

--- src/Arrays/LC_75_SortColors.java
+++ src/Arrays/LC_75_SortColors.java
@@ -1,4 +1,23 @@
 package Arrays;
 
 public class LC_75_SortColors {
+    public void sortColors(int[] nums) {
+        int count=0;
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0)count++;
+            else if(nums[i]==1)count1++;
+            else count2++;
+        }
+        for(int i=0;i<count;i++){
+            nums[i]=0;
+        }
+        for(int i=count;i<count+count1;i++){
+            nums[i]=1;
+        }
+        for(int i=count+count1;i<nums.length;i++){
+            nums[i]=2;
+        }
+    }
 }
