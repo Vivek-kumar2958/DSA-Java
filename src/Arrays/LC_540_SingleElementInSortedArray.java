5f4143bae89bffa6c0316a65abb028459d7a8370
vivek kumar <vivekkumar2958bxr@gmail.com>
2026-08-17 00:03:41 +0530

    Add LC 540 single elment in sorted array

--- src/Arrays/LC_540_SingleElementInSortedArray.java
+++ src/Arrays/LC_540_SingleElementInSortedArray.java
@@ -1,6 +1,6 @@
 package Arrays;
 
-public class LC_Singleelement {class Solution {
+public class LC_540_SingleElementInSortedArray {class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n=0;
         for(int i=0;i<nums.length;i++){
